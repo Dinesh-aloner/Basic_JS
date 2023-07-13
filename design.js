f6500c7aeb11791cd0e4af622faba3aa0d415010
Dinesh-aloner <128777909+Dinesh-aloner@users.noreply.github.com>
2023-07-13 14:47:46 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,9 @@
+var a1=parseInt(prompt("Enter a first number"));
+var a2=parseInt(prompt("Enter a second  number"));
+var op = prompt("Enter the operator");
+switch(op){
+    case '+':document.getElementsById("result").textContent=(a1+a2);break;
+    case '-':document.getElementById("result").textContent=(a1-a2);break;
+    case '*':document.getElementById("result").textContent=(a1*a2);break;
+    case '/':document.getElementById("result").textContent=(a1/a2);break;
+}
